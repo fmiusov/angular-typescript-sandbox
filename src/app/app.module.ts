1d65146641ed4536ab1a587fff0f6240a2dca009
Jayson Duff <jayson.duff@gsgus.com>
2022-05-03 04:39:10 -0400

    two way binding added with forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { UserComponent } from "./user.component";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [AppComponent, UserComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
