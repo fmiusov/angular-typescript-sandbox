9ef4ec8fb8c68b99312b4b1656a1e33d39efc412
Jayson Duff <jayson.duff@gsgus.com>
2022-05-03 04:27:58 -0400

    reactive method added to custom component in export class

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -2,9 +2,15 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-user",
-  template: `<p>Hello {{ name }}!</p>
+  template: `
+    <input type="text" (input)="onUserInput($event)">
+    <p>Hello {{ name }}!</p>
     <p>I'm the user component</p>`,
 })
 export class UserComponent {
-  name = 'Jayson'
+  name = "Jayson";
+
+  onUserInput(event) {
+    this.name = event.target.value;
+  }
 }
