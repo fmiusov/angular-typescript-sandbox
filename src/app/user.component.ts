6808285ea1f6d031e16014418922ee9217121007
Jayson Duff <jayson.duff@gsgus.com>
2022-05-03 04:13:05 -0400

    custom component added with template and name property

---
+++ src/app/user.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-user",
+  template: `<p>Hello {{ name }}!</p>
+    <p>I'm the user component</p>`,
+})
+export class UserComponent {
+  name = 'Jayson'
+}
