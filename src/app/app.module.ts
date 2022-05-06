a616f54984e66ed54ef6a0cde75cb0d8b2d95856
Jayson Duff <jayson.duff@gsgus.com>
2022-05-06 05:02:17 -0400

    scoped styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { UserComponent } from "./user.component";
 import { FormsModule } from "@angular/forms";
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, UserComponent],
+  declarations: [AppComponent, UserComponent, UserDetailComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
